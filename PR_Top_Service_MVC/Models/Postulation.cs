f845b76f60fe358d5984eb6c710a6f9c67af57da
David <teranjegre@gmail.com>
2023-04-08 22:52:44 -0400

    logica login implementada, verificacion en la base de datos lista

---
+++ PR_Top_Service_MVC/Models/Postulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PR_Top_Service_MVC.Models
+{
+    public partial class Postulation
+    {
+        public int IdPostulation { get; set; }
+        public string Description { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public string Address { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public int IdProfessional { get; set; }
+
+        public virtual Profesional IdProfessionalNavigation { get; set; } = null!;
+    }
+}
--- PR_Top_Service_MVC/Program.cs
+++ PR_Top_Service_MVC/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PR_Top_Service_MVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TopServiceBDOContext>(options =>
+options.UseSqlServer(
+	builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 var app = builder.Build();
 
